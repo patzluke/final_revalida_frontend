4f119b46c683739cdb79904ff092c2ce1c1c0423
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-08-15 15:33:46 +0800

    nomyr commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/farmer/farmer.module').then(m => m.FarmerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
