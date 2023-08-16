eb1c7cc767c8752844888bfa6a743176eb3d6ea6
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-08-16 19:01:06 +0800

    initial wholesaler commit

---
+++ src/app/modules/wholesaler/wholesaler.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WholesalerRoutingModule } from './wholesaler-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WholesalerRoutingModule
+  ]
+})
+export class WholesalerModule { }
