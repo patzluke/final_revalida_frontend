60713650062ad3c495a46526547b4824a7bc98c5
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-08-17 12:29:50 +0800

    initial course commit

--- src/app/modules/course/course.module.ts
+++ src/app/modules/course/course.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseComponent } from './components/course/course.component';
 import { CourseRoutingModule } from './course-routing.module';
-import { AddCourseComponent } from './components/add-course/add-course.component';
 import { EditCourseComponent } from './components/edit-course/edit-course.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddCourseComponent } from './components/add-course/add-course.component';
 
 
 
