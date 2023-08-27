fc2d90d229f08f187489e19573ce956f256613a6
Leo Cruz <118149927+cruzleo@users.noreply.github.com>
2023-08-27 20:54:53 +0800

    edited loader component

--- src/app/modules/shared/components/loader/loader.component.ts
+++ src/app/modules/shared/components/loader/loader.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss']
+  styleUrls: ['./loader.component.scss'],
 })
-export class LoaderComponent {
+export class LoaderComponent implements OnInit {
+  constructor(private el: ElementRef) {}
+  isLoading = true;
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1500);
+  }
 }
