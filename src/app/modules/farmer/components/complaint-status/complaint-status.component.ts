d0087411016b2c39546a651ad8e0ea29cbd96662
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-08-31 12:20:18 +0800

    add crop complaint status farmer complaint commit

--- src/app/modules/farmer/components/complaint-status/complaint-status.component.ts
+++ src/app/modules/farmer/components/complaint-status/complaint-status.component.ts
@@ -1,3 +1,4 @@
+import { addFarmerComplaintState } from './../../states/farmercomplaint-state/farmercomplaint.actions';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FarmerComplaintState } from '../../states/farmercomplaint-state/farmercomplaint.reducer';
@@ -7,7 +8,7 @@ import { FarmerComplaint } from '../../models/farmercomplaint';
 import { Router } from '@angular/router';
 import { faSave, faCancel, faAdd, faEye } from '@fortawesome/free-solid-svg-icons';
 import Swal from 'sweetalert2';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TableModule } from 'primeng/table';
 
 @Component({
@@ -20,9 +21,8 @@ export class ComplaintStatusComponent implements OnInit {
   itemsPerPage: number = 8; // Number of items to show per page
 
   complaintForm!: FormGroup
-  addComplaintForm!: FormGroup
 
-  farmerComplaint: FarmerComplaint[] = []
+  farmerComplaints: FarmerComplaint[] = []
 
   loading: boolean = true;
 
@@ -44,17 +44,69 @@ export class ComplaintStatusComponent implements OnInit {
 
   faEye = faEye;
 
-  farmerComplaints: FarmerComplaint[] = [];
 
-  //selectores
+  imageUrl: string | ArrayBuffer | null = null;
+
+  onImageSelect(event: any): void {
+
+    const file = event.target.files[0];
+    if (file) {
+      // Read the file and generate a data URL
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  addFarmerComplaint() {
+    if (this.complaintForm.valid) {
+      let complaintFormValues = this.complaintForm.value;
+      let newComplaintFormValues: FarmerComplaint = {
+        complaintTitle: complaintFormValues.complaintTitle,
+        complaintMessage: complaintFormValues.complaintMessage,
+        image: complaintFormValues.image,
+        farmerId: localStorage.getItem('userId') as any
+      }
+      Swal.fire({
+        title: 'Are you sure you want to file this complaint?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Send offer',
+      }).then(
+        (result) => {
+          if (result.isConfirmed) {
+            this.store.dispatch({
+              type: FarmerComplaintActions.ADD_FARMERCOMPLAINT,
+              addFarmerComplaint: newComplaintFormValues
+            })
+          }
+        }
+      )
+      this.complaintForm.reset()
+    }
+  }
+
+  //selectors
   selectFarmerComplaints$ = this.store.select(selectFarmerComplaints());
 
   constructor(
     private store: Store<FarmerComplaintState>,
-    private _router: Router
-  ) { }
+    private _router: Router,
+    private builder: FormBuilder
+  ) {
+    this.complaintForm = builder.group({
+      complaintTitle: ['', Validators.required],
+      complaintMessage: ['', Validators.required],
+      image: [null],
+      farmerId: ['', Validators.required],
+    })
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.store.dispatch({
       type: FarmerComplaintActions.GET_SINGLE_FARMERCOMPLAINTS,
       farmerId: localStorage.getItem('userNo'),
@@ -62,6 +114,7 @@ export class ComplaintStatusComponent implements OnInit {
 
     this.selectFarmerComplaints$.subscribe((data) => {
       this.farmerComplaints = data;
+      this.loading = false
     });
   }
 
@@ -72,6 +125,8 @@ export class ComplaintStatusComponent implements OnInit {
   navigateToEditComplaint(farmerComplaintId: any) {
     this._router.navigateByUrl(`/farmer/complaints/edit/${farmerComplaintId}`);
   }
+
+
 
   cancelComplaint(farmerComplaintId: any) {
     Swal.fire({
