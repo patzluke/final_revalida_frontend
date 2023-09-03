94e447a801873d011e93436e2d0b25c23d081e31
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-09-04 07:28:08 +0800

    deleted order summary at farmer side commit

--- src/app/modules/farmer/farmer.module.ts
+++ src/app/modules/farmer/farmer.module.ts
@@ -46,7 +46,6 @@ import { CourseEffects } from './states/course-state/course.effects';
 import { TabViewModule } from 'primeng/tabview';
 import { coursesEnrolledReducer } from './states/course-enrolled-state/course-enrolled.reducer';
 import { CourseEnrolledEffects } from './states/course-enrolled-state/course-enrolled.effects';
-import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
 import { cropPaymentReducer } from './states/crop-payment-state/crop-payment.reducer';
 import { CropPaymentEffects } from './states/crop-payment-state/crop-payment.effects';
 
@@ -63,8 +62,7 @@ import { CropPaymentEffects } from './states/crop-payment-state/crop-payment.eff
     CourseEnrolledComponent,
     ViewReportComponent,
     ProfileComponent,
-    CoursesComponent,
-    OrderSummaryComponent,
+    CoursesComponent
   ],
   imports: [
     CommonModule,
