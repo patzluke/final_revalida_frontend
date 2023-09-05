ac2474ae721cd9283f42422190ab0141d52fb4cb
Nomyr Julz Deomampo <ndeomampo@77global.biz>
2023-09-06 07:32:13 +0800

    hihi commit

--- src/app/modules/wholesaler/wholesaler.module.ts
+++ src/app/modules/wholesaler/wholesaler.module.ts
@@ -32,12 +32,9 @@ import { postAdvertisementReducerSupplierSide } from './states/postadvertisement
 import { PostAdvertisementEffectsSupplierSide } from './states/postadvertisement-state/postadvertisement.effects';
 import { NotificationsComponent } from './components/notifications/notifications.component';
 import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
-<<<<<<< HEAD
 import { OrderListComponent } from './components/order-list/order-list.component';
-=======
 import { cropPaymentReducer } from './states/crop-payment-state/crop-payment.reducer';
 import { CropPaymentEffects } from './states/crop-payment-state/crop-payment.effects';
->>>>>>> 1c4fa9afaf7653d79f62415ed07f86d9a614a33d
 
 @NgModule({
   declarations: [
